f896ee317a7f71300f27f10732a670007bfca04e
Marco Ariel Dutto <138625903+arieldutto@users.noreply.github.com>
2025-12-08 19:23:22 -0300

    Mejoro el componente dropdown

--- sgisp3/LEERME.md
+++ sgisp3/LEERME.md
@@ -1,2 +1,2 @@
-Como trabajo final se esta adaptando un sistema que estaba en produccion a React.
-se trabajo con la autenticacion de firebase, hay un usuario test@test.com.ar y pass test123
+Como trabajo final se esta adaptando un sistema que estaba en producción a React.
+se trabajo con la autenticación de firebase, hay un usuario test@test.com.ar y pass test123
--- sgisp3/src/components/DropdownMenu.jsx
+++ sgisp3/src/components/DropdownMenu.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
-
 function DropdownMenu({ title, items = [], align = "start", icon = null }) {
     return (
-        <>
-            <li className="nav-item dropdown">
-                <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                >
-                    {icon && <i className={`bi ${icon}`}></i>} {title}
-                </a>
+        <li className="nav-item dropdown">
+            <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
+                {icon && <i className={`bi ${icon}`}></i>} {title}
+            </a>
 
-                <ul className={`dropdown-menu dropdown-menu-${align}`}>
-                    {items.map((item, index) => (
-                        <li key={index}>
+            <ul className={`dropdown-menu dropdown-menu-${align}`}>
+                {items.map((item, index) => (
+                    <li key={index}>
+                        {item.action ? (
+                            /** ITEM CON ACCIÓN */
+                            <button
+                                className="dropdown-item"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    item.action();
+                                }}
+                            >
+                                {item.label}
+                            </button>
+                        ) : (
+                            /** ITEM NORMAL CON HREF */
                             <a className="dropdown-item" href={item.href}>
                                 {item.label}
                             </a>
-                        </li>
-                    ))}
-                </ul>
-            </li>
-        </>
-    )
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </li>
+    );
 }
 
-export default DropdownMenu
+export default DropdownMenu;
