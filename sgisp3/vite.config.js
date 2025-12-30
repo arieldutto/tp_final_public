face91edae9c81ed25fff6f0502e07e021fd4d2e
arieldutto <marcoarieldutto@gmail.com>
2025-12-30 00:56:47 +0000

    Snapshot versión funcional para TP final

--- sgisp3/vite.config.js
+++ sgisp3/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,        // <-- permite conexiones externas
+    port: 5173,        // <-- opcional, el puerto de Vite
+    strictPort: true   // <-- evita que cambie de puerto
+  }
+
 })
